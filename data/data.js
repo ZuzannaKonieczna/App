af5ee31e0b7bdd4ed00e9474f8211b7560d03ae6
koniexzna <zuzannakonieczna29@gmail.com>
2025-01-17 19:22:08 +0100

    17/01/20205

---
+++ data/data.js
@@ -0,0 +1,17 @@
+const wiedza_o_szczepie = {
+  wiedza_o_szczepie:[
+    {
+    id: 1,
+    question: "Kto jest komendantem?",
+    anwser: "Imie i nazwisko"
+    },
+    {
+      id: 2,
+      question: "Ilu było komendantów?",
+      anwser: "6"
+    }
+  ]
+
+}
+
+export default wiedza_o_szczepie;
